21f43a14f0657cbeeded411c97077527ef68065f
Javier-Ovalles-92 <46820287+Javier-Ovalles-92@users.noreply.github.com>
2022-03-09 17:09:23 -0400

    proyecto de react terminado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Contador from './components/Contador';
+import ContadorMejorado from './components/ContadorMejorado';
+import CrudApi from './components/CrudApi';
+import ShopingCart from './components/ShopingCart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>useReducer</h1>
+    <hr/>
+    <CrudApi/>
+    <hr/>
+    <ShopingCart/>
+       <hr/>
+    <ContadorMejorado/>
+       <hr/>
+     <Contador/>
+  </div>
   );
 }
 
